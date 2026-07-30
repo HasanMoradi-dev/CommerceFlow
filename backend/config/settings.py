50f6e9a639fb10e2ff4a21ca05ce5e0efdc95efa
amir <amirpasha500@gmail.com>
2026-07-30 20:01:18 +0330

    Add Cloudinary

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'djoser',
     'rest_framework.authtoken',
 
+    'cloudinary',
+    'cloudinary_storage',
+
 ]
 
 MIDDLEWARE = [
@@ -163,3 +166,12 @@ CORS_ALLOWED_ORIGINS = [
     "https://commerceflow-ashy.vercel.app",
     "https://commerceflow-1v0li5teo-hm-dev1.vercel.app",
 ]
+
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
