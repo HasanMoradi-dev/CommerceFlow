ead73c4ca7c7bbc92cf8b82c2d830ae78bffe971
amir <amirpasha500@gmail.com>
2026-07-30 14:17:57 +0330

    Temporarily allow all hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -155,6 +155,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",),
 }
-ALLOWED_HOSTS = ["localhost", "127.0.0.1" ,"172.28.164.226"]
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173" ,"http://172.28.164.226:5173"]
